98f8b21ecb64328f921293d6c23c27ee56f86560
DAppzy <stoneridgebali@gmail.com>
2025-01-06 03:04:13 +0800

    fix: update vite config for better asset handling

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,21 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return 'assets/images/[name]-[hash][extname]';
+          }
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
       },
     },
   },
